71c30954076f54b8c76a4c17828a3dd5e79de800
unknown <melbadawy07@gmail.com>
2026-03-04 10:31:44 -0800

    end my project rayz shop maping test basurl

--- app/(frontend)/lib/helper/fetcherBasUrl.ts
+++ app/(frontend)/lib/helper/fetcherBasUrl.ts
@@ -1,10 +1,8 @@
 import { headers } from "next/headers";
 
-export async function getBaseUrl() {
-  if (typeof window !== "undefined") {
-    return "";
-  }
-  const host = (await headers().then((res)=>res)).get("host");
+export  function getBaseUrl() {
+
+  const host = process.env.BASE_URL;
 //   const head = (await headers().then((res)=>res)).get("referer");
-  return `http://${host}`
+  return host
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import HomePage from "./(frontend)/components/HomePage/HomePage";
 
 export default function Home() {
   return (
-<>
-<HomePage/>
-</>
+    <>
+      <HomePage />
+    </>
   );
 }
